f4dd184934ade1cbced1ae9d3f97dde760c615cd
Farooq AR <farooqbckk@gmail.com>
2016-07-14 09:20:14 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+    $('#search-toggle').on('click', function() {
+        $('#search-input').addClass('open').focus();
+
+    });
+    $('#search-input').on('keyup', toggleClear);
+    $('#search-input').on('keyup', onSubmit);
+    $('#search-clear').on('click', clearInput);
+
+    $('.random-article-btn').on('click',getRandomArticle);
+    function clearInput() {
+        $('#search-input').focus().val('');
+        toggleClear();
+
+    }
+
+    function showLoader() {
+        $('#search-input').attr('disabled', 'disabled');
+        $('.loader').addClass('show');
+        $('#search-clear').removeClass('show');
+    }
+
+    function hideLoader() {
+        var input_length = $('#search-input').val().length;
+        $('#search-input').removeAttr('disabled');
+        $('.loader').removeClass('show');
+        if (input_length > 0) {
+            $('#search-clear').addClass('show');
+        }
+        $('#search-input').focus();
+    }
+
+    function toggleClear() {
+        var input_length = $('#search-input').val().length;
+        if (input_length > 0) {
+            $('#search-clear').addClass('show');
+
+        } else {
+            $('#search-clear').removeClass('show');
+        }
+        hideError();
+    }
+
+    function showError(errorText) {
+        $('#error').text(errorText).show();
+    }
+
+    function hideError() {
+        $('#error').hide();
+    }
+
+    function showResult(result) {
+        var html = '<div class="result">' +
+            '<a target="_blank" href="https://en.wikipedia.org/?curid=' + result['pageid'] + '">' +
+            '<h2>' + result['title'] + '</h2>' +
+            '<p>' + result['extract'] + '</p>' +
+            '</a>' +
+            '</div>';
+        $('#search-results').append(html);
+    }
+
+    function clearResult() {
+        $('#search-results').empty();
+    }
+
+    function onSubmit(event) {
+        var input = $('#search-input').val();
+        if (event.keyCode === 13 && input.length > 0) {
+            showLoader();
+            $.ajax({
+                crossDomain: true,
+                dataType: 'jsonp',
+                type: 'POST',
+                url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + encodeURIComponent(input),
+                success: function(data) {
+                    var query = data['query'];
+                    clearResult();
+                    if (query == undefined) {
+                        showError('No result matches the query');
+                    } else {
+
+                        for (var i in query['pages']) {
+                            var result = query['pages'][i];
+                            console.log(result);
+                            showResult(result);
+                        }
+                    }
+                    hideLoader();
+                },
+                error: function() {
+                    hideLoader();
+                    clearResult();
+                    showError('An Error occurred!');
+                }
+            });
+        }
+    }
+
+    function getRandomArticle() {
+        hideError();
+        $.ajax({
+            crossDomain: true,
+            dataType: 'jsonp',
+            type: 'POST',
+            url: 'https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=100&format=json',
+            success: function(data) {
+                var query = data['query'];
+                clearResult();
+
+                for (var i in query['pages']) {
+                    var result = query['pages'][i];
+                    showResult(result);
+                }
+            },
+            error: function() {
+                clearResult();
+                showError('An Error occurred!');
+            }
+        });
+    }
+
+});
